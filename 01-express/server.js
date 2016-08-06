0a8b4336fee34df0ac6f93f58534789ab4d825a1
dannycrews <dancrews@me.com>
2016-08-06 19:37:46 -0400

    Add middleware and jade

--- 01-express/server.js
+++ 01-express/server.js
@@ -6,9 +6,35 @@ var http = require("http"),
 // create express
 const app = express();
 
+app.set("view engine", "jade");
+
+// browser -> node -> express -> M1 -> M2 -> handler
+//         <-      <-         <-    <-    <-
+
+
+app.use((request, response, next) => {
+    console.log("In middleware 1");
+    next();
+    console.log("Out of middleware 1");
+});
+
+app.use(express.static("./public")); 
+
+
+app.use((request, response, next) => {
+    console.log("---In middleware 2");
+    next();
+    console.log("---Out of middleware 2");
+});
+
 // config express
 app.get("/", (request, response) => {
     response.end("Hello World");
+    console.log("In handler"); 
+});
+
+app.get("/home", (request, response) => {
+    response.render("index", {title: "TITLE!"});
 });
 
 //create server
